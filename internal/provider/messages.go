246fc1ce7a1e6f141ad1e623a43e9f6bd3580e5e
Hector Valcarcel <hmvalcarcel@gmail.com>
2025-06-11 11:18:36 +0200

    feat: simplify provider helpers and centralize error messages
    - Centralize common error and validator messages in `messages.go`
    
    Signed-off-by: Hector Valcarcel <hmvalcarcel@gmail.com>

---
+++ internal/provider/messages.go
@@ -0,0 +1,66 @@
+package provider
+
+// common error messages.
+const (
+	msgErrGetPgConnection         = "Error establishing a PostgreSQL connection."
+	msgErrStartPgTransaction      = "Error starting a PostgreSQL transaction."
+	msgErrCommitPgTransaction     = "Failed to commit PostgreSQL transaction."
+	msgErrorExecutingPgAction     = "Error %s PostgreSQL '%s'."
+	msgErrorMissingResId          = "Missing resource ID in the request."
+	msgErrorParsingProviderData   = "Error parsing provider data."
+	msgErrorPgObjectNotFund       = "PostgreSQL object '%s' not found."
+	msgErrorPgObjectNotFundDetail = "The PostgreSQL '%s' with ID '%s' was not found in the current connection."
+)
+
+// common validator messages.
+const (
+	msgRequiredField             = "This field '%s' is required for the resources '%s'."
+	msgInvalidFieldPattern       = "The value received for the field '%s' is invalid."
+	msgInvalidFieldPatternDetail = "The expected format for the field '%s' is '%s'."
+	msgInvalidType               = "Expected type: '%s', received instead: '%T'."
+)
+
+type PostgresqlObjectType int
+
+const (
+	PGUserFunction PostgresqlObjectType = iota
+	PGRole
+	PGEventTrigger
+)
+
+func (p PostgresqlObjectType) String() string {
+	switch p {
+	case PGUserFunction:
+		return "User Function"
+	case PGRole:
+		return "Role"
+	case PGEventTrigger:
+		return "Event Trigger"
+	default:
+		return "unknown"
+	}
+}
+
+type TerraformAction int
+
+const (
+	TFCreateAction TerraformAction = iota
+	TFDeleteAction
+	TFReadAction
+	TFUpdateAction
+)
+
+func (t TerraformAction) String() string {
+	switch t {
+	case TFCreateAction:
+		return "Create"
+	case TFDeleteAction:
+		return "Delete"
+	case TFReadAction:
+		return "Read"
+	case TFUpdateAction:
+		return "Update"
+	default:
+		return "unknown"
+	}
+}
